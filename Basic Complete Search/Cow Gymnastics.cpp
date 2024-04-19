4c5446e0d534107d8c4c56e5916b6f1d8ad859be
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-19 10:59:26 +0200

    Create Cow Gymnastics.cpp

---
+++ Basic Complete Search/Cow Gymnastics.cpp
@@ -0,0 +1,59 @@
+//problem link: https://usaco.org/index.php?page=viewproblem2&cpid=963
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define F first
+#define S second
+#define PB push_back
+
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void solve(){
+	int k,n; cin >> k >> n;
+	int a[k][n];
+	for(int i = 0; i < k; i++){
+		for(int j = 0; j < n; j++){
+			cin >> a[i][j];
+		}
+	}
+	
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = i+1; j < n; j++){
+			bool ok = true;
+			for(int x = 1; x < k; x++){
+				for(int y = 0; y < n; y++){
+					if(a[x][y]==a[0][i]){
+						break;
+					}
+					if(a[x][y]==a[0][j]){
+						ok = false;
+						break;
+					}
+				}
+				if(!ok)break;
+			}
+			if(ok)ans++;
+		}
+	}
+	cout << ans << '\n';
+}
+
+int main(){
+	ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	setIO("gymnastics");
+	int testcase = 1;
+	// cin >> testcase;
+	for(int i = 1; i <= testcase; i++){
+		// cout << "Case " << i << ": ";
+		solve();
+	}
+} 	 		   			  	
